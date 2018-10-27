046b77d40e5d7592f1c49ecd1a59beeb59aae906
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-10-27 19:14:44 -0300

    Cleanup active repository on search

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,8 @@ import api from '@/utils/api';
 
 export default {
   async SEARCH_REPOSITORIES({ commit }, username) {
+    commit('SET_ACTIVE_REPOSITORY', {});
+
     const data = await api.searchRepositories(username);
 
     commit('SET_REPOSITORIES', data);
