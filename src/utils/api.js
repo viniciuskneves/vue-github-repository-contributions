e1e3ba4b5a68ff288d4d2abe5c258a499d9d503a
Vinicius Kiatkoski Neves <kneves.vinicius@gmail.com>
2018-10-27 18:33:24 -0300

    API method implementation

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,29 @@ function buildSearchRepositoriesURL(username) {
   return `https://api.github.com/users/${username}/repos`;
 }
 
+function buildSearchContributorsURL(repository) {
+  return `https://api.github.com/repos/${repository}/contributors`;
+}
+
 export default {
   async searchRepositories(username) {
     try {
       const response = await fetch(buildSearchRepositoriesURL(username));
+
+      if (!response.ok) {
+        throw response;
+      }
+
+      return response.json();
+    } catch (exception) { // Ignore any possible errors
+      console.log(exception);
+
+      return [];
+    }
+  },
+  async searchContributors(repository) {
+    try {
+      const response = await fetch(buildSearchContributorsURL(repository));
 
       if (!response.ok) {
         throw response;
